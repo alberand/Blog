cbd5e09c336424f4fe2502e08b06b6436386aa98
alberand <albershteyn.andrey@gmail.com>
2020-06-27 14:18:03 +0200

    fix: update domain in config

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://alberand.github.io'
+SITEURL = 'https://alberand.io'
 RELATIVE_URLS = False
 
 DELETE_OUTPUT_DIRECTORY = False
